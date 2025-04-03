d3a27349570e436ba76de7db5f7f7b35dd65a545
alina513 <130853290+alina513@users.noreply.github.com>
2025-04-03 12:06:51 +0300

    Update numbers.js

--- backend/numbers.js
+++ backend/numbers.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const NumberSchema = new Schema(
   {
     number: {
-      type: Number,
+      type: String,
       required: [true, "Number is required"],
     },
   },
